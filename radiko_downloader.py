40ebb5850d80a7aa3f1654187f0c0f4aec12467f
Sam Phung <sphung@uncc.edu>
2024-06-30 19:12:11 -0400

    Escape & and update headers

--- radiko_downloader.py
+++ radiko_downloader.py
@@ -1,9 +1,14 @@
 import subprocess
 import re
 
+import requests
+
+#TODO: M3U8 URL IS IN WEBPAGE and can be found by searching for ID "20230218000000" in URL "https://radiko.jp/#!/ts/INT/20230218000000"
+#<input type="hidden" id="url" value="https://radiko.jp/v2/api/ts/playlist.m3u8?station_id=INT&amp;l=15&amp;ft=20230218000000&amp;to=20230218003000">
 
 # the m3u8 url
 url = input("URL: ")
+url = url.replace("&", "`&")
 title = input("Filename: ")
 artist = input("Artist: ")
 # e.g. 2021-10-12 or 20211012
@@ -60,15 +65,31 @@ for line in content:
 # re.sub(r'[^-\w\.]', '_', description)
 #If there is an '&' then escape it using "`&" in both python and powershell
 description = description.replace('&', '"`&"')
+
 print(description)
 
-area_id = input("X-Radiko-AreaId: ")
 auth_token = input("X-Radiko-AuthToken: ")
 
+if auth_token is None or auth_token == " ":
+    auth1_url = "https://radiko.jp/v2/api/auth1"
+    x_radiko_app = "aSmartPhone7a"
+    x_radiko_app_version = "7.3.8"
+    x_radiko_device = "29.SGP712"
+    x_radiko_user = "71f7949557fded5c65e21a03ccd44af0"
+
+    header = {'x-radiko-app': x_radiko_app, 'x-radiko-app-version': x_radiko_app_version, 'x-radiko-device': x_radiko_device, 'x-radiko-user': x_radiko_user}
+    try:
+        auth1_res = requests.get(url=auth1_url, headers=header)
+        auth_token = auth1_res.headers['x-radiko-authtoken']
+    except Exception as e:
+        print(e)
+        input("Press any key to exit: ")
+        sys.exit
+
 ffmpeg_args = ['powershell.exe', '-NoExit', 'ffmpeg']
-ffmpeg_args += ["-headers", f'"X-Radiko-AreaId: {area_id}"', "-headers", f'"X-Radiko-AuthToken: {auth_token}"']
+ffmpeg_args += ["-headers", f'"X-Radiko-AreaId: JP13"', "-headers", f'"X-Radiko-AuthToken: {auth_token}"']
 # ffmpeg_args += ['-user_agent', '"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"']
-ffmpeg_args += ['-i', f'"{url}"', '-metadata', f'date={date}', '-metadata', f"artist='{artist}'",
+ffmpeg_args += ['-i', url, '-metadata', f'date={date}', '-metadata', f"artist='{artist}'",
                 '-metadata', f'description="{description}"', '-codec', 'copy', f"'{fileName}'"]
 try:
     process = subprocess.run(ffmpeg_args)
